073d4a54860ece480a614c4415ec73ff9b25a27b
Mahfoudh AROUS <mahfoudh.arous@gmail.com>
2020-04-14 19:11:29 +0100

    adding plugin page

--- custom-admin-menus.php
+++ custom-admin-menus.php
@@ -1,12 +1,13 @@
 <?php
 /*
-Plugin Name: Custom Admin Menus
+Plugin Name: Remove Admin Menus
 Description: this my first plugin.
 Author: Mahfoudh Arous
 Version: 0.1
 Author URI: https://aladindev.com
 */
 
+/*
 function remove_admin_menus(){
 	 //remove_menu_page( 'index.php' );                  //Dashboard
 	 //remove_menu_page( 'jetpack' );                    //Jetpack* 
@@ -22,3 +23,67 @@ function remove_admin_menus(){
    }
 
 add_action( 'admin_menu', 'remove_admin_menus' );
+*/
+
+function ramdev_init() {
+	register_setting( 'ramdev_settings', 'ramdev', 'ramdev_validate' );
+}
+add_action( 'admin_init', 'ramdev_init' );
+
+function ram_options_page() {
+	?>
+		<div class="wrap">
+			<?php screen_icon(); ?>
+			<h2><?php _e( 'Remove Admin Menus', 'ram' ); ?></h2>
+			<p><?php _e( 'This plugin helps you remove admin menus.', 'ram' ); ?></p>
+			<form method="POST" action="options.php">
+				<table class="form-table">
+					<tbody>
+						<?php ramdev_do_options(); ?>
+					</tbody>
+				</table>
+				<input type="submit" class="button-primary" value="<?php _e( 'Save Options', 'ps-demo' ); ?>" />
+				<input type="hidden" name="ps-plugindev-submit" value="Y" />
+			</form>
+		</div>
+	<?php
+}
+
+
+function ramdev_do_options() {
+	$options = get_option( 'ramdev' );
+	ob_start();
+	?>
+		<tr valign="top"><th scope="row"><?php _e( 'What menus do you want to remove?', 'ram' ); ?></th>
+			<td>
+				<?php
+					foreach ( ramdev_services() as $service ) {
+						$label = $service['label'];
+						$value = $service['value'];
+						echo '<label><input type="checkbox" name="ramdev[' . $value . '] value="1" ';
+						switch ($value) {
+							case 'appearence' :
+								if ( isset( $options['appearence'] ) ) { checked( 'on', $options['appearence'] ); }
+								break;
+						}
+						echo '/> ' . esc_attr($label) . '</label><br />';
+					}
+				?>
+			</td>
+	<?php
+}
+
+function ramdev_services() {
+	$services = array(
+		'appearence' => array(
+			'value' => 'appearence',
+			'label' => __( 'Appearence', 'ram' )
+		)
+	);
+	return $services;
+}
+
+function my_plugindev_menu() {
+	add_submenu_page( 'options-general.php', __( 'Remove Admin Menus', 'ram' ), __( 'Remove Admin Menus', 'ram' ), 'administrator', 'ram_dev', 'ram_options_page' );
+}
+add_action( 'admin_menu', 'my_plugindev_menu' );
